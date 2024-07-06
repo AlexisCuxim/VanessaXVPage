69ea51a662c81bb9feecbc2476ad29b59a53e779
Alexis Cuxim Poot <alexiscuximpoot@Laptop-de-Alexis.local>
2024-07-06 18:54:29 -0500

    react-router-dom integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from './pages/Home';
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/vanessa-sanchez" element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
