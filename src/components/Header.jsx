f0607eaf656f0ba0610a19de35d5532af5f7509e
Alexis Cuxim Poot <alexiscuximpoot@Laptop-de-Alexis.local>
2024-07-25 21:50:18 -0500

    z-10 audio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
         <source src={CreeYAtrevete} type="audio/mpeg"/>
       </audio>
       <span 
-        className="material-symbols-outlined text-white relative left-1/2 -translate-x-1/2 text-[3rem] top-8 cursor-pointer"
+        className="material-symbols-outlined text-white relative left-1/2 -translate-x-1/2 text-[3rem] top-8 cursor-pointer z-[10]"
         onClick={handleAudioControl}
       >
         { isPlaying ? 'pause_circle' : 'play_circle' }
